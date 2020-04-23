c69ee0a86f0499e90334ebf73ac98b1b34a5c1b2
prnxv <53377336+prnxv@users.noreply.github.com>
2020-04-24 01:01:22 +0530

    spa1

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,72 +1,65 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardBody, CardText, CardTitle } from "reactstrap";
 
-class Dishdetail extends Component {
-  constructor(props) {
-    super(props);
+function RenderComments({ comments }) {
+  if (comments === null) {
+    return <div></div>;
   }
+  const comms = comments.map((comm) => {
+    return (
+      <li key={comm.id}>
+        <p>{comm.comment}</p>
+        <p>
+          -- {comm.author}, &nbsp;
+          {new Intl.DateTimeFormat("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "2-digit",
+          }).format(new Date(comm.date))}
+        </p>
+      </li>
+    );
+  });
+  return (
+    <div className="col-12 col-md-5 m-1">
+      <h4> Comments </h4>
+      <ul className="list-unstyled">{comms}</ul>
+    </div>
+  );
+}
 
-  renderComments(comments) {
-    if (comments === null) {
-      return <div></div>;
-    }
-    const comms = comments.map((comm) => {
-      return (
-        <li key={comm.id}>
-          <p>{comm.comment}</p>
-          <p>
-            -- {comm.author}, &nbsp;
-            {new Intl.DateTimeFormat("en-US", {
-              year: "numeric",
-              month: "long",
-              day: "2-digit",
-            }).format(new Date(comm.date))}
-          </p>
-        </li>
-      );
-    });
+function RenderDish({ dish }) {
+  if (dish != null) {
     return (
       <div className="col-12 col-md-5 m-1">
-        <h4> Comments </h4>
-        <ul className="list-unstyled">{comms}</ul>
+        <Card>
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
       </div>
     );
-  }
-
-  renderDish(dish) {
-    if (dish != null) {
-      return (
-        <div className="col-12 col-md-5 m-1">
-          <Card>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
-        </div>
-      );
-    } else {
-      return <div></div>;
-    }
-  }
-
-  render() {
-    const dish = this.props.dish;
-    if (dish == null) {
-      return <div></div>;
-    }
-    const dishItem = this.renderDish(dish);
-    const commentItem = this.renderComments(dish.comments);
-    return (
-      <div className="container">
-        <div className="row">
-          {dishItem}
-          {commentItem}
-        </div>
-      </div>
-    );
+  } else {
+    return <div></div>;
   }
 }
+
+const Dishdetail = (props) => {
+  if (props.dish == null) {
+    return <div></div>;
+  }
+  const dishItem = <RenderDish dish={props.dish} />;
+  const commentItem = <RenderComments comments={props.dish.comments} />;
+  return (
+    <div className="container">
+      <div className="row">
+        {dishItem}
+        {commentItem}
+      </div>
+    </div>
+  );
+};
 
 export default Dishdetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,41 +1,51 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+import Home from "./HomeComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
 import DishDetail from "./DishdetailComponent";
 import { DISHES } from "./shared/dishes";
+import { COMMENTS } from "./shared/comments";
+import { PROMOTIONS } from "./shared/promotions";
+import { LEADERS } from "./shared/leaders";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS,
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
-  }
-
   render() {
+    const HomePage = () => {
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu
-          dishes={this.state.dishes}
-          onClick={(dishId) => this.onDishSelect(dishId)}
-        />
-        <DishDetail
-          dish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
       </div>
     );
   }
